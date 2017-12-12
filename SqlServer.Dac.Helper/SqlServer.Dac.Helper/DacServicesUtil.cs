843a529b7857d26c5339d3614a830d6ee3d0e8f2
Konstantin Fukszon <fukszon.kostya@gmail.com>
2017-12-11 16:13:03 -0800

    Some more initiali commits

---
+++ SqlServer.Dac.Helper/SqlServer.Dac.Helper/DacServicesUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dac;
+
+namespace SqlServer.Dac.Helper
+{
+    public class DacServicesUtil
+    {
+        private DacServices ds;
+        SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+
+        public DacServicesUtil(string servername)
+        {
+            sb.IntegratedSecurity = true;
+            sb.DataSource = servername;
+            sb.Pooling= false;
+            ds = new DacServices(sb.ConnectionString);
+        }
+
+        public void Deploy(string dbname, string dacpacpath, DacDeployOptions options)
+        {
+            var dp = DacPackage.Load(dacpacpath);
+            ds.Deploy(dp, dbname, true, options);
+        }
+    }
+}
--- SqlServer.Dac.Helper/SqlServer.Dac.Helper/Program.cs
+++ SqlServer.Dac.Helper/SqlServer.Dac.Helper/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Dac;
+using Mono.Options;
 
 namespace SqlServer.Dac.Helper
 {
@@ -10,6 +12,26 @@ namespace SqlServer.Dac.Helper
     {
         static void Main(string[] args)
         {
+            var sqlserver = string.Empty;
+            var dacpacpath = string.Empty;
+            var dbname = string.Empty;
+            var method = string.Empty;
+            bool help = false;
+            var p = new OptionSet()
+            {
+                { "h|?|help", "show this message and exit.", v=> help = v != null},
+                {"m|method=", "the method to call on the helper method. Available options: create, deploy, remove, ", v=> method = v },
+                {"s|sqlserver=", "the FQDN/Alias of the SQL Server Instance.", v=> sqlserver = v },
+                {"p|path:", "the path to the DacPac File.", v=> dacpacpath = v },
+                {"d|database:", "the name of the database for deployment.", v=> dbname = v }
+            };
+            p.Parse(args);
+
+            
+
+
+            if (help)
+                p.WriteOptionDescriptions(Console.Out);
         }
     }
 }
